64391e9b6fd3db9eb1a52c22f34f1fc600082a18
Dane LeGassick <danelegassick@gmail.com>
2023-01-23 18:30:34 -0500

    Testing + IDM 241 + IDM232
    added 232 public html and idm241 folder

---
+++ test-zone/components/zoom-image.php
@@ -0,0 +1,7 @@
+<div class="test-wrap01">
+  <div class="js-scroll-size-up">
+    <div class="js-scroll-size-up__item">
+      <img src="https://picsum.photos/id/10/1600/900" alt="" width="1600" height="900">
+    </div>
+  </div>
+</div>
---
+++ test-zone/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>    
+
+    <h1><strong>Scroll to 100% of area size</strong><br> Vanilla JS (requestAnimationFrame)</h1>
+
+<p>scroll<br>↓</p>
+
+
+<?php
+
+include 'components/zoom-image.php'
+
+?>
+
+</body>
+</html>
